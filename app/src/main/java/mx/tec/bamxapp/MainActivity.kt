0f93fdcfaf949a920bf9ebfac7c14aade31a01c2
Jonathan Maya <yony_ms@live.com>
2021-10-19 23:59:05 -0500

    a mimir

--- app/src/main/java/mx/tec/bamxapp/MainActivity.kt
+++ app/src/main/java/mx/tec/bamxapp/MainActivity.kt
@@ -5,16 +5,24 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var sharedPreferences: SharedPreferences
     lateinit var sharedPreferences1: SharedPreferences
+    lateinit var queue: RequestQueue
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -43,23 +51,57 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val user = "Palencia"
                 println("Diste click al boton Login")
 
-                if(edt_user.text.toString() == user && edt_password.text.toString() == key){
-                    val intent = Intent(this@MainActivity, MainMenu::class.java)
+                login()
+
+//                if(edt_user.text.toString() == user && edt_password.text.toString() == key){
+//                    val intent = Intent(this@MainActivity, MainMenu::class.java)
+//                    with(sharedPreferences.edit()){
+//                        putString("usuario", edt_user.text.toString())
+//                        putBoolean("logIn", true)
+//                        commit()
+//                    }
+//                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+//                    startActivity(intent)
+//                    println("Datos correctos")
+//                }
+//                else{
+//                    edt_password.setText("")
+//                    println("Datos Incorrectos")
+//                    Toast.makeText(this@MainActivity, "Datos Incorrectos", Toast.LENGTH_SHORT).show()
+//                }
+            }
+        }
+    }
+
+    private fun login(){
+        queue = Volley.newRequestQueue(this)
+        val credenciales = JSONObject()
+        credenciales.put("email", edt_user.text.toString())
+        credenciales.put("password", edt_password.text.toString())
+
+        val loginRequest = JsonObjectRequest(
+            Request.Method.POST,
+            "http://bamx.denissereginagarcia.com/public/login",
+            credenciales,
+            Response.Listener { response ->
+                Log.e("VolleyResponseTrue", response.toString())
+                val intent = Intent(this@MainActivity, MainMenu::class.java)
                     with(sharedPreferences.edit()){
-                        putString("usuario", edt_user.text.toString())
+                        putString("usuario", response.getString("name"))
                         putBoolean("logIn", true)
                         commit()
                     }
                     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                     startActivity(intent)
                     println("Datos correctos")
-                }
-                else{
-                    edt_password.setText("")
-                    println("Datos Incorrectos")
-                    Toast.makeText(this@MainActivity, "Datos Incorrectos", Toast.LENGTH_SHORT).show()
-                }
+            },
+            { error ->
+                Log.e("VolleyResponse", error.toString())
+                edt_password.setText("")
+                println("Datos Incorrectos")
+                Toast.makeText(this@MainActivity, "Datos Incorrectos", Toast.LENGTH_SHORT).show()
             }
-        }
+        )
+        queue.add(loginRequest)
     }
 }
